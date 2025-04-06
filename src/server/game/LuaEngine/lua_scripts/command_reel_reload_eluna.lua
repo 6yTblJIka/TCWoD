d18dd6b470cfad2c8a81f9338a53a32be66b79d1
fluxurion <zoltan@fatalerror.hu>
2025-04-06 19:41:16 +0200

    Eluna: Set CMake to be able to install default scripts
    - Added 2 default scripts

---
+++ src/server/game/LuaEngine/lua_scripts/command_reel_reload_eluna.lua
@@ -0,0 +1,8 @@
+-- ".reload eluna" also works
+local function ElunaReload (event, player, command)
+	if (command == "reel") then
+		ReloadEluna()
+		player:CastSpell( player, 29422, 1 )
+	end
+end
+RegisterPlayerEvent(42, ElunaReload)
